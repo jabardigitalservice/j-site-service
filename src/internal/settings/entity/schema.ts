1014e3c792a3c90e87466ae7fb3802aa131d7ded
ayocodingit <ayocodingit@gmail.com>
2023-05-04 11:55:22 +0700

    adjust validate domain and favicon

--- src/internal/settings/entity/schema.ts
+++ src/internal/settings/entity/schema.ts
@@ -1,8 +1,8 @@
 import Joi from 'joi'
 
 export const Store = Joi.object({
-    domain: Joi.string().required(),
-    favicon: Joi.string().uri().optional().default(null),
+    domain: Joi.string().uri().required(),
+    favicon: Joi.string().uri().required(),
     logo: Joi.string().uri().required(),
     color_palatte: Joi.string().required(),
     name: Joi.string().required(),
